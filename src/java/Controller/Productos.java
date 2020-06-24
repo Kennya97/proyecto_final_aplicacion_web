fa2e7b53f90cac375f9359bb44aac87c5c8bd828
Willian <williamhernandez450@gmail.com>
2020-06-24 17:29:27 -0600

    Controlador Productos, Moficacion en prueba

--- src/java/Controller/Productos.java
+++ src/java/Controller/Productos.java
@@ -6,8 +6,6 @@ import DAO.ProductoDAOImplementar;
 import Model.Producto;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.Instant;
-import java.util.Date;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,72 +13,141 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author W. Hernandez
- */
+
 public class Productos extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Productos</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Productos at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String parametro = request.getParameter("opcion"); //Capturar el parametro que se esta enviando
-        if (parametro.equals("crear")) { //Evaluar si el parametro es crear o listar o cualquier otro
-            String pagina = "/Vistas-Productos/crearProducto.jsp"; //Vista o formulario para registrar una nueva categoria
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
-            dispatcher.forward(request, response);
-       }else{
-            this.listaProductos(request, response);
-        }
-    }
-    
     protected void listaProductos(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
+        throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        //Crear instancia de CategoriaDAO
-        ProductoDAO producto = new ProductoDAOImplementar();
-        //Crear instancia de session; se le da true para crear la sesion
-        HttpSession sesion = request.getSession(true);
-        sesion.setAttribute("lista", producto.Listar2()); //Lista es el nombre de la sesion
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Productos/listarProductos.jsp");
-        dispatcher.forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Producto producto = new Producto(); //Agregar las demas propiedades
-        //Se efectua el casting o conversion de datos porque lo ingresado en el formulario es texto
-        producto.setId_producto(Integer.parseInt(request.getParameter("idproducto")));
-        producto.setNom_producto(request.getParameter("txtnombre"));
-        producto.setDes_producto(request.getParameter("txtdescripcion"));
-        producto.setStock(Float.parseFloat(request.getParameter("txtstock")));
-        producto.setPrecio(Float.parseFloat(request.getParameter("txtprecio")));
-        producto.setUnidadMedida(request.getParameter("txtunidad"));
-        producto.setEstado(Integer.parseInt(request.getParameter("txtestado")));
-        producto.setCategoria_id(Integer.parseInt(request.getParameter("idcategoria")));
-        producto.setFecha_entrada(request.getParameter("fecha"));
         
-        ProductoDAO guardaproducto = new ProductoDAOImplementar();
-        guardaproducto.guardarPro(producto);
-        this.listaProductos(request, response);
-    }
+//Crear instancia a CategoriaDAO
+ProductoDAO producto = new ProductoDAOImplementar();
+//Crear instancia de sesión; se le da true para crear la sesión.
+
+HttpSession session = request.getSession(true);
+
+session.setAttribute("lista", producto.Listar()); //lista es el nombre de la variable de sesión.
+
+RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Productos/listarProductos.jsp");
+dispatcher.forward(request, response);
+
+
+}
+    
+protected void borrarProducto(HttpServletRequest request, HttpServletResponse response)
+ throws ServletException, IOException {
+response.setContentType("text/html;charset=UTF-8");
+    
+        
+     
+RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Productos/listarProductos.jsp");
+dispatcher.forward(request, response);
+}
+    
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+response.setContentType("text/html;charset=UTF-8");
+        
+}
+
+    
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+
+//Se captura el parámetro que se esta enviando.
+String parametro = request.getParameter("opcion");
+
+        
+//Evaluar si el parámetro es crear o listar o cualquier otro.
+        
+if(parametro.equals("crear")){
+            
+//Vista o formulario para registrar nueva categoria.
+            
+String pagina = "/Vistas-Productos/crearProducto.jsp";
+
+RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+dispatcher.forward(request, response);
+            
+}else if(parametro.equals("listar")){
+this.listaProductos(request, response);
+            
+}else if(parametro.equals("modificar")){
+            
+//Se efectua el casting o conversión de datos porque lo ingresado en el formulario es texto.
+int id_producto = Integer.parseInt(request.getParameter("id_pro"));
+String nombre_pro = request.getParameter("nombre_pro");
+String desc_pro = request.getParameter("descuento_pro");
+int id_catpro = Integer.parseInt(request.getParameter("cat_pro"));
+float stock_pro = Float.parseFloat(request.getParameter("stock_pro"));
+float precio_pro = Float.parseFloat(request.getParameter("precio_pro"));
+String unidad_pro = request.getParameter("unidad_pro");
+int estado_pro = Integer.parseInt(request.getParameter("estado_pro"));
+String fecha_pro = request.getParameter("fecha_pro");
+
+            
+String pagina = "/Vistas-Productos/crearProducto.jsp?id_p="+id_producto+"&&nombre_p="+nombre_pro+"&&descuento_p="+desc_pro+"&&idcat_p="+id_catpro+
+        "&&stock_p="+stock_pro+"&&precio_p="+precio_pro+"&&unidad_p="+unidad_pro+"&&estado_p="+estado_pro+"&&fecha_p="+fecha_pro+"&&senal=1";
+RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+dispatcher.forward(request, response);
+            
+}else if(parametro.equals("eliminar")){
+int del_id = Integer.parseInt(request.getParameter("id"));
+ProductoDAO producto = new ProductoDAOImplementar();
+producto.borrarPro(del_id);    
+this.listaProductos(request, response);
+}
+        
+}
+
+    
+@Override
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+ throws ServletException, IOException {
+  
+        
+Producto producto = new Producto();
+
+
+//Se efectua el casting o conversión de datos porque lo ingresado en el formulario es texto.
+
+int id_producto = Integer.parseInt(request.getParameter("id_producto"));
+String nombre_pro = request.getParameter("txtnombreproducto");
+String desc_pro = request.getParameter("txtdescuentoproducto");
+int id_catpro = Integer.parseInt(request.getParameter("txtidcatproducto"));
+float stock_pro = Float.parseFloat(request.getParameter("txtstockproducto"));
+float precio_pro = Float.parseFloat(request.getParameter("txtprecioproducto"));
+String unidad_pro = request.getParameter("txtunidadproducto");
+int estado_pro = Integer.parseInt(request.getParameter("txtestadoproducto"));
+String fecha_pro = request.getParameter("txtfechaproducto");
+      
+producto.setId_producto(id_producto);
+producto.setNom_producto(nombre_pro);
+producto.setDes_producto(desc_pro);
+producto.setCategoria_id(id_catpro); //dudas con el valor de la llave foranea
+producto.setStock(stock_pro);
+producto.setPrecio(precio_pro);
+producto.setUnidadMedida(unidad_pro);
+producto.setEstado(estado_pro);
+producto.setFecha_entrada(fecha_pro);
+
+
+//IMPLEMEMENTAR LA CLASE CategoriaDAO
+
+ProductoDAO guardarProducto = new ProductoDAOImplementar();
+
+guardarProducto.guardarPro(producto);
+        
+this.listaProductos(request, response);
+        
+}
+
+    
+@Override
+public String getServletInfo() {
+return "Short description";
+}// </editor-fold>
+
 
 }
