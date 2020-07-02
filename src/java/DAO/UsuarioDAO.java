dfc3d9156f66e9e09d097090bfec2d6e70f2f5af
CarlosAlfred20 <carlosalfredoalvarezlopez7001@gmail.com>
2020-07-02 01:12:02 -0600

    modifique usuarioDAO Signed-off-by:CarlosAlfred20 <carlosalfredoalvarezlopez7001@gmail.com>

--- src/java/DAO/UsuarioDAO.java
+++ src/java/DAO/UsuarioDAO.java
@@ -2,7 +2,12 @@
 package DAO;
 
 import Model.Usuario;
+import Model.Validar;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -14,5 +19,17 @@ public List<Usuario> Listar2();
 public Usuario editarUsu(int id_usu_edit);
 public boolean guardarUsu(Usuario usuario);
 public boolean borrarUsu(int id_usu_borrar);
+public int validar(Usuario usu)throws SQLException;
+ 
  
 }
+/*
+public class UsuarioDAO implements Validar{
+    Connection con;
+    Conexion cn = new Conexion();
+    PreparedStAtement ps;
+    ResultSet rs;
+      
+   
+}
+*/
