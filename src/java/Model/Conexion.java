5bd674a4ebf1e91ddda126d8ee471fc3261b0c8d
CarlosAlfred20 <carlosalfredoalvarezlopez7001@gmail.com>
2020-07-02 01:03:21 -0600

    cree el modelo conexion Signed-off-by:CarlosAlfred20 <carlosalfredoalvarezlopez7001@gmail.com>

--- src/java/Model/Conexion.java
+++ src/java/Model/Conexion.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//Conexio 
 public class Conexion {
     Connection con;
     public Connection getConnection(){
