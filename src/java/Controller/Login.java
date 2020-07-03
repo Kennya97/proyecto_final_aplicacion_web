e399502c1b201fe5dd1339357471621c3bbc589f
CarlosAlfred20 <salinasjulissa6@gmail.com>
2020-07-02 22:07:30 -0600

    Actualizado el proyecto

---
+++ src/java/Controller/Login.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.UsuarioDAO2;
+import Model.Usuarios;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author DELL 1
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("txtCorreo");
+	String password = request.getParameter("txtPass");
+        UsuarioDAO2 userDao = new UsuarioDAO2();
+		
+		try {
+			Usuarios user = userDao.VerLogin(email, password);
+			String destPage = "index.jsp";
+			
+			if (user != null) {
+                            String corr = user.getCorreo();
+                            String pass = user.getClave();
+                            String name = user.getNombre();
+                            String ape = user.getApellido();
+                            
+                            String contal =corr+pass;
+                            String nameto = name+" "+ape;
+                            String contal2 = corr;
+                            HttpSession session = request.getSession();
+                                //accion que servira para confirmar si este cumple con la sesion
+                                
+				session.setAttribute("userd", contal);
+                                session.setAttribute("userd2", contal2);
+                                session.setAttribute("name", nameto);
+				destPage = "Principal.jsp";
+			} else {
+				String message = "Invalid email/password";
+				request.setAttribute("message", message);
+			}
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+			dispatcher.forward(request, response);
+			
+		} catch (SQLException | ClassNotFoundException ex) {
+			throw new ServletException(ex);
+		}
+                
+                
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/DAO/UsuarioDAO2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Controller.Usuario;
+import Factory.ConexionDB;
+import Factory.FactoryConexionDB;
+import Model.Usuarios;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author DELL 1
+ */
+public class UsuarioDAO2 {
+    ConexionDB conn;
+    
+    public UsuarioDAO2(){
+           //Llama al factoryh para ejeuctar secuencias SQL
+          this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
+    }
+    public Usuarios VerLogin(String email, String password)throws SQLException,ClassNotFoundException{
+       String jdbcURL = "jdbc:mysql://localhost:3306/bd_inventario";
+		String dbUser = "root";
+		String dbPassword = "";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+		String sql = "SELECT * FROM tb_usuario WHERE correo = ? and clave = ?";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, email);
+		statement.setString(2, password);
+
+		ResultSet result = statement.executeQuery();
+
+		Usuarios user = null;
+
+		if (result.next()) {
+			user = new Usuarios();
+			//user.(result.getString("fullname"));
+			user.setCorreo(result.getString("correo"));
+                        user.setNombre(result.getString("nombre"));
+                        user.setApellido(result.getString("apellido"));
+		}
+
+		connection.close();
+
+		return user;
+    
+    }
+    
+    
+    
+}
